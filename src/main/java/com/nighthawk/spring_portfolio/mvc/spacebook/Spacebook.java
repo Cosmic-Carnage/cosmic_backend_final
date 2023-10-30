d9ff6cf56fd4b3229eb0be864d1cba08ba81b35f
Tay Kim <taykimpro@gmail.com>
2023-10-29 17:58:03 -0700

    editting getters and setters

--- src/main/java/com/nighthawk/spring_portfolio/mvc/spacebook/Spacebook.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/spacebook/Spacebook.java
@@ -23,27 +23,6 @@ import lombok.NoArgsConstructor;
 @Component
 @Configuration
 
-public class ModelInit {
-    @Autowired
-    private JokesJpaRepository jokesRepo;
-    @Autowired
-    private LeaderboardJpaRepository leaderboardRepo;
-    @Autowired
-    private SpacebookJpaRepository spacebookRepo;
-    @Autowired
-    private NoteJpaRepository noteRepo;
-    @Autowired
-    private PersonDetailsService personService;
-
-    @Bean
-    public CommandLineRunner run() {
-        return args -> {
-            // Your initialization logic here
-        };
-    }
-}
-
-@Entity
 public class Spacebook {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -57,44 +36,4 @@ public class Spacebook {
 
     @Lob
     private byte[] image;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getSpacebook() {
-        return spacebook;
-    }
-
-    public void setSpacebook(String spacebook) {
-        this.spacebook = spacebook;
-    }
-
-    public Integer getLike() {
-        return like;
-    }
-
-    public void setLike(Integer like) {
-        this.like = like;
-    }
-
-    public Integer getDislike() {
-        return dislike;
-    }
-
-    public void setDislike(Integer dislike) {
-        this.dislike = dislike;
-    }
-
-    public byte[] getImage() {
-        return image;
-    }
-
-    public void setImage(byte[] image) {
-        this.image = image;
-    }
 }

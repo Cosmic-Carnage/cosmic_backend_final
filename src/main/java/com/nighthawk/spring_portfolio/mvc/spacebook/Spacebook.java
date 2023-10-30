ef692d28c8e06241f49a272f0e3cc40b0b13c33d
realethantran <etran8@gmail.com>
2023-10-29 19:00:25 -0700

    port 8027

--- src/main/java/com/nighthawk/spring_portfolio/mvc/spacebook/Spacebook.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/spacebook/Spacebook.java
@@ -1,39 +1,39 @@
-package com.nighthawk.spring_portfolio.mvc.spacebook;
+// package com.nighthawk.spring_portfolio.mvc.spacebook;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.stereotype.Component;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
-import jakarta.persistence.OneToOne;
-import javassist.Loader.Simple;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Lob;
+// import jakarta.persistence.OneToOne;
+// import javassist.Loader.Simple;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Component
-@Configuration
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Entity
+// @Component
+// @Configuration
 
-public class Spacebook {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+// public class Spacebook {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.AUTO)
+//     private Long id;
 
-    @Column(unique = true)
-    private String spacebook;
+//     @Column(unique = true)
+//     private String spacebook;
 
-    private Integer like;
-    private Integer dislike;
+//     private Integer like;
+//     private Integer dislike;
 
-    @Lob
-    private byte[] image;
-}
+//     @Lob
+//     private byte[] image;
+// }

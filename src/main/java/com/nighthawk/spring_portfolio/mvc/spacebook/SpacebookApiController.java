58d56fe661c420adb53037f3c6659447e82dc978
realethantran <etran8@gmail.com>
2023-11-01 16:40:37 -0700

    Spacebook Major Changes + Tweaks

--- src/main/java/com/nighthawk/spring_portfolio/mvc/spacebook/SpacebookApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/spacebook/SpacebookApiController.java
@@ -1,100 +1,58 @@
+
 package com.nighthawk.spring_portfolio.mvc.spacebook;
 
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-// import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import org.springframework.ui.Model;
-import java.io.IOException;
-import java.util.Base64;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import java.io.File;
-
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/spacebook")
+@RequiredArgsConstructor
+@RequestMapping("/image")
 public class SpacebookApiController {
+    @Autowired
+    private SpacebookJpaRepository uploadFileRepository;
 
     @Autowired
-    private SpacebookJpaRepository spacebookRepo;
+    SpacebookApiAppl imageService;
 
-    @GetMapping("/")
-    public ResponseEntity<List<Spacebook>> getSpacebook() {
-        return new ResponseEntity<>(spacebookRepo.findAll(), HttpStatus.OK);
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @PostMapping
+    public ResponseEntity<String> save(MultipartFile image, @RequestParam("fileName") String fileName) throws IOException {
+        String encodedString = Base64.getEncoder().encodeToString(image.getBytes());
+        Spacebook file = new Spacebook(fileName, encodedString);
+        uploadFileRepository.save(file);
+        return new ResponseEntity<>("Image has been uploaded", HttpStatus.CREATED);
     }
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> saveSpacebookToDB(@RequestParam("file") String file) {
-        if (file.isEmpty()) {
-            return new ResponseEntity<>("Please select a file to upload.", HttpStatus.BAD_REQUEST);
-        }
-
-        try {
-            byte[] imageBytes = Base64.getDecoder().decode(file);
-            Spacebook spacebook = new Spacebook();
-            spacebook.setImage(imageBytes);
-
-            Spacebook savedSpacebook = spacebookRepo.save(spacebook);
-
-            return new ResponseEntity<>("Image uploaded successfully. Spacebook ID: " + savedSpacebook.getId(), HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("Upload failed.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<byte[]> serveImage(@PathVariable("id") Long id) {
-        Optional<Spacebook> optional = spacebookRepo.findById(id);
+    @GetMapping("/{fileName}")
+    public ResponseEntity<byte[]> downloadImage(@PathVariable String fileName) {
+        Optional<Spacebook> optional = uploadFileRepository.findByfileName(fileName);
         if (optional.isPresent()) {
-            Spacebook spacebook = optional.get();
-            byte[] imageBytes = spacebook.getImage();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.IMAGE_JPEG); // Set the content type to image/jpeg or your file type
-            return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+            Spacebook file = optional.get();
+            String data = file.getImageEncoder();
+            byte[] imageBytes = Base64.getDecoder().decode(data);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .contentType(MediaType.valueOf("image/png"))
+                    .body(imageBytes);
         } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
-    }
-
-    @PostMapping("/like/{id}")
-    public ResponseEntity<Spacebook> setUpVote(@PathVariable long id) {
-        Optional<Spacebook> optional = spacebookRepo.findById(id);
-        if (optional.isPresent()) { 
-            Spacebook spacebook = optional.get(); 
-            spacebook.setLike(spacebook.getLike()+1);
-            spacebookRepo.save(spacebook); 
-            return new ResponseEntity<>(spacebook, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
-    }
-    @PostMapping("/dislike/{id}")
-    public ResponseEntity<Spacebook> setDownVote(@PathVariable long id) {
-        Optional<Spacebook> optional = spacebookRepo.findById(id);
-        if (optional.isPresent()) { 
-            Spacebook spacebook = optional.get();
-            spacebook.setDislike(spacebook.getDislike()+1);
-            spacebookRepo.save(spacebook);
-            return new ResponseEntity<>(spacebook, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Spacebook> deleteSpacebook(@PathVariable long id) {
-        Optional<Spacebook> optional = spacebookRepo.findById(id);
-        if (optional.isPresent()) {
-            Spacebook spacebook = optional.get();
-            spacebookRepo.deleteById(id);
-            return new ResponseEntity<>(spacebook, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }

8017c14eeb2e9840fb9e6fbfd7a79d6f8da20c35
Tay Kim <taykimpro@gmail.com>
2023-11-01 17:02:56 -0700

    getting rid of warnings

--- src/main/java/com/nighthawk/spring_portfolio/mvc/quizleaderboard/QuizLeaderboard.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/quizleaderboard/QuizLeaderboard.java
@@ -2,7 +2,6 @@ package com.nighthawk.spring_portfolio.mvc.quizleaderboard;
 
 import java.util.HashMap;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
